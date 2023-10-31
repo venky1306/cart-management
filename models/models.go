c49c39667c66870abf3800a07e718fe547830040
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-10-30 23:40:53 -0500

    added user type

--- models/models.go
+++ models/models.go
@@ -8,11 +8,12 @@ import (
 
 type User struct {
 	ID              primitive.ObjectID `json:"_id" bson:"_id"`
-	First_Name      *string            `json:"first_name"  	validate:"required,min=2, max=30"`
-	Last_Name       *string            `json:"last_name"   	validate:"required,min=2, max=30"`
-	Password        *string            `json:"password"    	validate:"required, min=6"`
-	Email           *string            `json:"email"		validate:"email,required"`
-	Phone           *string            `json:"phone"		validate:"required"`
+	First_Name      *string            `json:"first_name" validate:"required,min=2, max=30"`
+	Last_Name       *string            `json:"last_name" validate:"required,min=2, max=30"`
+	Password        *string            `json:"password" validate:"required, min=6"`
+	Email           *string            `json:"email" validate:"email,required"`
+	Phone           *string            `json:"phone" validate:"required"`
+	User_Type       *string            `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
 	Token           *string            `json:"token"`
 	Refresh_Token   *string            `json:"refresh_token"`
 	Created_At      time.Time          `json:"created_at"`
