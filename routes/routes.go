5a8c32894e88918e24d55ad8a28a065e2f2de6d0
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-10-30 23:42:11 -0500

    removed /admin/add-product from open api's

--- routes/routes.go
+++ routes/routes.go
@@ -8,7 +8,6 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/signup", controllers.SignUp())
 	incomingRoutes.POST("/users/login", controllers.Login())
-	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewAdmin())
 	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
 	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
