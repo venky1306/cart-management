55afa6d34aa4d790ab637d276257031911452eaa
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-11-27 00:29:16 -0600

    refactored code to have only one address for a account

--- controllers/address.go
+++ controllers/address.go
@@ -1,19 +1,58 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
 
-func AddAddress() gin.HandlerFunc {
+	"github.com/gin-gonic/gin"
 
-}
+	"github.com/venky1306/cart-management/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
-func EditHomeAddress() gin.HandlerFunc {
+func EditAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userid := c.Query("userID")
+		if userid != "" {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "userID missing"})
+		}
 
-}
+		var addr models.Address
+		if err := c.BindJSON(&addr); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
 
-func EditWordAddress() gin.HandlerFunc {
+		validateErr := validate.Struct(addr)
+		if validateErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validateErr})
+			return
+		}
 
-}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*100)
+		defer cancel()
 
-func DeleteAddress() gin.HandlerFunc {
+		var user models.User
+
+		err := UserCollection.FindOne(ctx, bson.M{"_id": userid}).Decode(&user)
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		addr.Address_ID = primitive.NewObjectID()
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		result, err := UserCollection.UpdateByID(ctx, bson.D{{Key: "_id", Value: userid}}, bson.D{{Key: "$set", Value: bson.D{{Key: "address", Value: addr}, {Key: "updtaed_at", Value: user.Updated_At}}}})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "unable to add address"})
+			return
+		}
+		c.JSON(http.StatusOK, result)
+	}
 
 }
